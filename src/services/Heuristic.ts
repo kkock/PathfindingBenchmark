7ef2433c3a64193dac9e9ca53f7ad344e6eaabca
k.kock <k.kock@deelnemer.ictmaatwerk.com>
2026-07-07 16:02:39 +0200

    Add unit-weight vacuum world heuristic.

--- src/services/Heuristic.ts
+++ src/services/Heuristic.ts
@@ -1,14 +1,21 @@
 import type { SearchDomain } from '../graph/Graph'
 import type { Point } from '../graph/GridGraph'
+import type { VacuumState, VacuumWorld } from '../graph/VacuumWorldGraph'
 import { euclideanDistance } from './misc'
 
-type HeuristicCallback<S> = (graph: SearchDomain<S>, state1: S, state2: S) => number
+type HeuristicCallback<
+  S,
+  G extends SearchDomain<S> = SearchDomain<S>
+> = (graph: G, state1: S, state2: S) => number
 
-export class Heuristic<S> {
-  public readonly get: HeuristicCallback<S>
+export class Heuristic<
+  S,
+  G extends SearchDomain<S> = SearchDomain<S>
+> {
+  public readonly get: HeuristicCallback<S, G>
   public readonly name: string
 
-  constructor (cb: HeuristicCallback<S>, name: string) {
+  constructor (cb: HeuristicCallback<S, G>, name: string) {
     this.get = cb
     this.name = name
   }
@@ -20,5 +27,17 @@ export function getWeightedHeuristic<S> (weight: number, heuristic: Heuristic<S>
   const cb: HeuristicCallback<S> = (...args) => weight * heuristic.get(...args)
   return new Heuristic<S>(cb, `${heuristic.name}(${weight})`)
 }
+
+export const vacuumHeuristic = new Heuristic<VacuumState, VacuumWorld>((graph, state1, _) => {
+  const points = [state1.robotPosition]
+  for (let i = 0; i < graph.dirtPositions.length; ++i) {
+    if (((state1.remaining >> i) & 1) !== 0) {
+      points.push(graph.dirtPositions[i]!)
+    }
+  }
+
+  const mst = graph.getMst(points)
+  return mst.reduce((a, b) => a + b)
+}, 'vacuum')
 
 export class InadmissibleHeuristic<S> extends Heuristic<S> {}
